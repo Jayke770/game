ff8ae5d863118c7f5e2bb8517fa7441ce4c8ada2
PSI 2 <psi2@teamdao.com>
2022-08-06 12:22:31 +0000

    .

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en" className='dark scroll-smooth'>
-            <Head />
+            <Head>
+                <meta name="theme-color" content="#27272a" />
+            </Head>
             <body className="overflow-hidden h-screen w-screen m-0 p-0 bg-zinc-800 flex justify-center items-center">
                 <Main />
                 <NextScript />
